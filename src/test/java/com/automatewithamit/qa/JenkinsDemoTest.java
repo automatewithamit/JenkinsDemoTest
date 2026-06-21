d78dee764ea5a9e09101f4d10abcaa66ac09fe50
amittripathi <automatewithamit@gmail.com>
2026-06-21 14:03:49 +0530

    initial commit

---
+++ src/test/java/com/automatewithamit/qa/JenkinsDemoTest.java
@@ -0,0 +1,22 @@
+package com.automatewithamit.qa;
+
+import org.testng.annotations.Test;
+
+import java.util.Map;
+
+public class JenkinsDemoTest {
+
+
+    @Test
+    public void testFlightSearch() {
+
+        System.out.println("Testing FlightSearch");
+
+
+    }
+    @Test
+    public void testRail(){
+        System.out.println("Testing TestRail");
+    }
+
+}
